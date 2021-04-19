65397621e2fa84be23ecf8f778ded4c3974bddf7
Freddy <atm64ioc@gmail.com>
2021-04-19 19:38:24 +0200

    Vue Basics (14)

--- Section-2/Vue-3-Firebase-lesson-5-6/app.js
+++ Section-2/Vue-3-Firebase-lesson-5-6/app.js
@@ -6,22 +6,29 @@ const app = Vue.createApp({
   /* template: '<h2>I am the template</h2>', */
   data() {
     return {
+      url: 'http://www.thenetninja.co.uk',
       showBooks: true,
       books: [
         {
           title: 'Name of the wind',
           author: 'Patrick Rothfuss',
           age: 45,
+          img: 'assets/1.jpg',
+          isFav: true,
         },
         {
           title: 'The way of kings',
           author: 'Brandon Sanderson',
           age: 45,
+          img: 'assets/2.jpg',
+          isFav: false,
         },
         {
           title: 'The final empire',
           author: 'Brandon Sanderson',
           age: 45,
+          img: 'assets/3.jpg',
+          isFav: true,
         },
       ],
       x: 0,
