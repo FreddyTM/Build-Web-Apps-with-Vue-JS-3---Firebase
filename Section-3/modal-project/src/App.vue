bb9e9d23771ef10831e7b2a7ed8062280b3d9f7c
Freddy <atm64ioc@gmail.com>
2021-04-20 13:15:14 +0200

    Vue CLI (21)

--- Section-3/modal-project/src/App.vue
+++ Section-3/modal-project/src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <!-- [vue/no-multiple-template-root]
+The template root requires exactly one element.eslint-plugin-vue -->
+
+  <!-- Ok I finally got it to work.
+I would suggest adding something to the docs for this:
+
+First install eslint and eslint-plugin-vue locally:
+
+npm install --dev eslint eslint-plugin-vue
+Then add a .eslintrc.js file in the root with the following settings:
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+  ],
+  plugins: ['vue'],
+} -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: 'My First Vue App :)',
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +43,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 2px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
