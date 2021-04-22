f55011fd6f8c7c3520de506501da5ba3ba8763cc
Freddy <atm64ioc@gmail.com>
2021-04-22 13:09:25 +0200

    Forms & data binding (43)

---
+++ Section-5/web-form/src/components/SignupForm.vue
@@ -0,0 +1,134 @@
+<template>
+  <form>
+    <!-- v-model binds data in both directions: the input value will bind to the property value
+    and the property value will bind to the input value -->
+    <label>Email:</label>
+    <input type="email" required v-model="email" />
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web developer</option>
+      <option value="designer">Web designer</option>
+    </select>
+    <!-- keeping track of user keypress to store the data in the input field to the skills array -->
+    <!-- we'll use keyup (key press and release), and the function addSkill will do the job -->
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkills" />
+    <!-- loop throug the skills array, each one generates a new div and have a key of its own value -->
+    <!-- the key must be unique, so we have to have this into consideration into the addSkills function -->
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <!-- if we ckick on the skill, it'll be deleted from the skills list (and won't be displayed either) -->
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+    <!-- the terms property will hold true or false depending of the checkbox being selected or not -->
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms &amp; conditions</label>
+    </div>
+    <!-- the array names will hold the values of the selected checkboxes, because all are referenced to that array -->
+    <div>
+      <input type="checkbox" value="shaun" v-model="names" />
+      <label>Shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="names" />
+      <label>Mario</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yoshi" v-model="names" />
+      <label>Yoshi</label>
+    </div>
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <p>Names: {{ names }}</p>
+  <p>Skills: {{ skills }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      /* If we set initial values to the properties, they will appear in the input fields
+      email: 'mario',
+      password: '1234', */
+      email: '',
+      password: '',
+      role: 'designer',
+      terms: false,
+      names: [],
+      tempSkill: '',
+      skills: [],
+    };
+  },
+  methods: {
+    addSkills(e) {
+      /* console.log(e); */
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill.slice(0, this.tempSkill.length - 1))) {
+          this.skills.push(this.tempSkill.slice(0, this.tempSkill.length - 1));
+        }
+        this.tempSkill = '';
+      }
+    },
+    deleteSkill(skill) {
+      console.log(skill);
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+</style>
