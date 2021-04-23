34aa4f13fe66eb962b46c5a4d44c766e5f15abfc
Freddy <atm64ioc@gmail.com>
2021-04-23 12:54:21 +0200

    Vue Router Basics (48)

--- Section-6/ninja-jobs/src/App.vue
+++ Section-6/ninja-jobs/src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
   <!-- any route selected will be injected where the <router-view> tag is -->
   <router-view />
