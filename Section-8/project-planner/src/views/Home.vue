a2ee8dac786e8d4aa73e184f5dd09303288d9f9a
Freddy <atm64ioc@gmail.com>
2021-04-26 11:15:35 +0200

    Simple Project Planner (59)

--- Section-8/project-planner/src/views/Home.vue
+++ Section-8/project-planner/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="home">
     Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <!-- <p>{{ project.title }}</p> -->
+        <SingleProject :project="project" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 /* import HelloWorld from '@/components/HelloWorld.vue' */
-
+import SingleProject from '@/components/SingleProject.vue';
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
