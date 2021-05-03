0dc7acc1149e0153ed8cf7441d7800ddbf38566c
Freddy <atm64ioc@gmail.com>
2021-05-03 19:58:24 +0200

    Live Chat Room (102)

---
+++ Section-13/live-chat/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHXcrlNEmuqeofgWyTD9v59JRcz6D5_24',
+  authDomain: 'udemy-vue-firebase-sites-6f2ae.firebaseapp.com',
+  projectId: 'udemy-vue-firebase-sites-6f2ae',
+  storageBucket: 'udemy-vue-firebase-sites-6f2ae.appspot.com',
+  messagingSenderId: '629012725587',
+  appId: '1:629012725587:web:26ab208e5337043bafeba6',
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+//init firebase service
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
